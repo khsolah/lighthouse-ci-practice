d569bce6eab1143106111f7d169366b9c08bbfae
khsolah <khsolah@gmail.com>
2022-02-01 03:03:34 +0800

    feat: 🎸 add windicss theme config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,22 @@ import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS()],
+  plugins: [
+    vue(),
+    WindiCSS({
+      config: {
+        theme: {
+          screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+            '2xl': '1536px'
+          }
+        }
+      }
+    })
+  ],
   resolve: {
     alias: [
       {
