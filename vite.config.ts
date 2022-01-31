1c644b06317c49e7b9040728cbef21222a9c99f2
khsolah <khsolah@gmail.com>
2022-02-01 04:40:02 +0800

    feat: 🎸 setup pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
@@ -17,6 +18,48 @@ export default defineConfig({
             '2xl': '1536px'
           }
         }
+      }
+    }),
+    VitePWA({
+      mode: (process.env as ImportMetaEnv).VITE_PWA_MODE,
+      base: (process.env as ImportMetaEnv).VITE_PWA_BASE,
+      includeAssets: ['favicon.ico', 'robots.txt'],
+      scope: '/',
+      srcDir: './src',
+      devOptions: {
+        enabled: (process.env as ImportMetaEnv).VITE_PWA_ENABLE_DEV === 'true',
+        /* when using generateSW the PWA plugin will switch to classic */
+        type: 'module',
+        navigateFallback: 'index.html'
+      },
+      manifest: {
+        name: 'Lighthouse CI Practice',
+        short_name: 'LHCI',
+        description: 'Practicing to use Lighthouse CI',
+        background_color: '#FFFFFF',
+        theme_color: '#FFFFFF',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png', // <== don't add slash, for testing
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png', // <== don't remove slash, for testing
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png', // <== don't add slash, for testing
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
       }
     })
   ],
