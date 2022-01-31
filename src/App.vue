b08b9fd9085ef53148368886ccf3673f5b5d32bc
khsolah <khsolah@gmail.com>
2022-02-01 04:38:48 +0800

    feat: 🎸 import reload prompt

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,15 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+
+// sw
+import ReloadPrompt from './components/sw/ReloadPrompt.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img alt="Vue logo" src="./assets/logo.png" class="w-50 h-50 m-auto" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <ReloadPrompt />
 </template>
 
 <style>
